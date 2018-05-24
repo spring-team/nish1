0aafe65cbe00d7b28ee8e6dd496aa84ac3a1d3c9
Rod Johnson <johnsonroda@gmail.com>
2018-05-24 15:13:21 +1000

    Update Nish1Controller.java

--- src/main/java/com/ippon/Nish1Controller.java
+++ src/main/java/com/ippon/Nish1Controller.java
@@ -27,7 +27,7 @@ class Nish1Controller {
 
     @RequestMapping(method = GET, path = "/")
     public String root() {
-        return "Spring Boot REST service running in the Woolshed Melbourne, Served by " + getClass().getName();
+        return "Spring Boot REST service running in the Woolshed Melbourne (after coffee), Served by " + getClass().getName();
     }
 
     @RequestMapping(method = GET, path = "hello/{name}")
