afd98d2b549e0ea5511fca6ffde8f137a19d77ad
Rod Johnson <johnsonroda@gmail.com>
2018-05-27 15:17:27 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/ippon/Nish1Application.java
+++ src/main/java/com/ippon/Nish1Application.java
@@ -1,3 +1,5 @@
+// Make new mistakes as fast as you can.
+
 // You're good enough, you're smart enough, and doggone it, people like you.
 
 // Our bodies and minds are capable of far more than our psyche will let us achieve.
