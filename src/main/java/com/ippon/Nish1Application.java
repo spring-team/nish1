c4610a2ceb79a9d25f5aa5f7b6a96d8edb773c36
Rod Johnson <johnsonroda@gmail.com>
2018-05-27 14:27:19 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/ippon/Nish1Application.java
+++ src/main/java/com/ippon/Nish1Application.java
@@ -1,5 +1,7 @@
 // You belong everywhere you are.
 
+// You belong everywhere you are.
+
 // No need to say 'I can do it.' You ARE doing it!
 
 // Life is its own reason for being. It is beautiful to have a sense of beauty.
