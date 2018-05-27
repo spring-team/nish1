644d1fb2ccfd5745804384b999bb88af986bc577
Rod Johnson <johnsonroda@gmail.com>
2018-05-27 14:26:52 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/ippon/Nish1Controller.java
+++ src/main/java/com/ippon/Nish1Controller.java
@@ -1,5 +1,7 @@
 // I believe in you. You can do the thing!
 
+// I believe in you. You can do the thing!
+
 // Our bodies and minds are capable of far more than our psyche will let us achieve.
 
 /*
